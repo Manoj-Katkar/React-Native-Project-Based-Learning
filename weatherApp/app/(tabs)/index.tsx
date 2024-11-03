373413e92b61992db3687d3741f7a10e6c6cc830
Manoj-Katkar <education2001manojkatkar@gmail.com>
2024-11-03 23:05:51 +0530

    learned about how to handle the images in react native

--- weatherApp/app/(tabs)/index.tsx
+++ weatherApp/app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, /* @tutinfo Import <CODE>StyleSheet</CODE> to define styles. */ StyleSheet } from 'react-native';
+import { Text, View, /* @tutinfo Import <CODE>StyleSheet</CODE> to define styles. */ StyleSheet  , Image} from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { TouchableOpacity } from 'react-native';
 
 import tw from 'twrnc';
 
+const logoImage = require("../../assets/images/react-logo.png");
+
 export default function Index() {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-black`}>
+    <View style={tw`flex-1 flex-col-reverse justify-center items-center bg-black`}>
       <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-lg`}>
       <Text style={tw`text-white text-3xl font-bold`}>Hi Manisha How Are You</Text>
 
+      {/* understanding how the images works in react native  */}
+      <Image source={logoImage}/>
+ 
       </TouchableOpacity>
       <StatusBar style='light'/>
     </View>
